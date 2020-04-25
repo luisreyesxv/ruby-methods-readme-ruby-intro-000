d0749dfbc4a9a14ea76ad28a17c3623fc13f8da2
Luis Reyes <luisreyesxv@yahoo.com>
2020-04-25 19:12:27 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,8 @@
+def greeting
+  puts "Hello world"
+  puts "My name is Soulja Boi Tell'em"
+  puts "and i just need you to know, that i'm so fresh"
+  puts "you can suck my balls"
+  puts "swag swag swag"
+end  
+
